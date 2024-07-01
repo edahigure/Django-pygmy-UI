7ba1a55d2f2991ac25681e27f3822229638661ad
edahi <edahigure@gmail.com>
2024-07-01 16:43:05 -0600

    Separate UI

---
+++ package/pygmyui/pygmyui/settings_tests.py
@@ -0,0 +1,11 @@
+# Pygmy Test Settings
+PYGMY_API_ADDRESS = 'django-mysqldb.cdmyq064u1wh.us-east-2.rds.amazonaws.com'
+
+PYGMY_API_AUTH = 'BasicAuth'
+
+PYGMY_API_USER = 'admin'
+
+PYGMY_API_PASSWORD = 'Ed_95611189'
+
+HOSTNAME = '127.0.0.1:8001'
+
